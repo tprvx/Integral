4b7b29f3ff61a945845958ce9224d60f73e40939
tprvx <petr_vaganov@mail.ru>
2021-01-02 21:54:56 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Работа_5
+{
+    public partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        //<param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Параметры интегрирования";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Левая граница";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(295, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Правая граница";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Green;
+            this.label4.Location = new System.Drawing.Point(12, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(218, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Кратность интегрирования";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(138, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(432, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(236, 77);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(60, 22);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "100";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(16, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(551, 30);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Ввести параметры интегрирования";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(763, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 30);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Расчет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(763, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 30);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "График";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(573, 154);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(316, 407);
+            this.panel1.TabIndex = 11;
+            this.panel1.Visible = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column4,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(16, 154);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(551, 407);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.Visible = false;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Шагов";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 80;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Приближенное значение";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Точное значение";
+            this.Column4.Name = "Column4";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Остаточный член";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 573);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+
+}
+
---
+++ Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Работа_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.RowCount = 10;
+        }
+
+        Integral I;
+        public Graphics Area;
+        public Pen P;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ввод параметров интегрирования
+            double A = Convert.ToDouble(textBox1.Text);
+            double B = Convert.ToDouble(textBox2.Text);
+            int M = Convert.ToInt32(textBox3.Text);
+            // создания объекта класса Integral
+            I = new Integral(A, B, M);
+            // скрытие элементов
+            this.label1.Visible = false;
+            this.label2.Visible = false;
+            this.label3.Visible = false;
+            this.label4.Visible = false;
+            this.textBox1.Visible = false;
+            this.textBox2.Visible = false;
+            this.textBox3.Visible = false;
+            this.button1.Visible = false;
+            this.button1.Visible = false;
+            // редактирование первой строки окна
+            this.Text += I;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int M0 = Convert.ToInt32(textBox3.Text);
+            for (int i = 0; i < 10; i++)
+            {
+                I.m = (int)((i + 1) / 10.0 * M0);
+                dataGridView1.Rows[i].Cells[0].Value = I.m;
+                dataGridView1.Rows[i].Cells[1].Value = I.ИнтСимпсон;
+                dataGridView1.Rows[i].Cells[2].Value = I.ИнтЛейбниц;
+                dataGridView1.Rows[i].Cells[3].Value = I.ОстаточныйЧленСимпсона;
+            }
+            dataGridView1.Visible = true;
+            button2.Visible = false;
+            button3.Visible = true;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Area = panel1.CreateGraphics();
+            int w = panel1.ClientSize.Width;
+            int h = panel1.ClientSize.Height;
+            P = new Pen(Color.Brown, 2);
+            Area.DrawLine(P, 0, 0, w, 0);
+            Area.DrawLine(P, 0, 0, 0, h);
+            Area.DrawLine(P, 0, h, w, h);
+            Area.DrawLine(P, w, 0, w, h);
+            P = new Pen(Color.DarkMagenta, 3);
+            Area.DrawLine(P, 10, 10, 10, h - 10);
+            Area.DrawLine(P, 10, h - 10, w - 10, h - 10);
+            Area.DrawString("f(x)",
+                new Font("Corier new", 12),
+                new SolidBrush(Color.DarkRed),
+                new Point(20, 10));
+            Area.DrawString("x",
+                new Font("Corier new", 12),
+                new SolidBrush(Color.DarkRed),
+                new Point(w - 20, h - 40));
+
+            float[] x = new float[11];
+            float[] fx = new float[11];
+            for (int i = 0; i <= 10; i++)
+            {
+                x[i] = i / 10F;
+                fx[i] = (float)I.fx(x[i]);
+            }
+            float fmax = fx[0];
+            foreach (float f in fx) if (fmax < f) fmax = f;
+            float My = (h - 20) / fmax;
+            float Mx = w - 20;
+            P = new Pen(Color.DarkGreen, 3);
+            float ix, iy, ix1 = 10 + x[0] * Mx, iy1 = h - 10 - fx[0] * My;
+            for (int i = 0; i < 10; i++)
+            {
+                ix = ix1;
+                iy = iy1;
+                ix1 = 10 + x[i + 1] * Mx;
+                iy1 = h - 10 - fx[i + 1] * My;
+                Area.DrawLine(P, ix, iy, ix1, iy1);
+            }
+            panel1.Visible = true;
+        }
+    }
+
+    class Integral
+    {
+        double A;
+        double B;
+        int M;
+        public Integral(double a, double b, int m)
+        {
+            A = a < b ? a : b;
+            B = b < a ? a : b;
+            M = m;
+        }
+
+        public int m
+        {
+            get { return M; }
+            set { M = value; }
+        }
+
+        public double fx(double x)
+        { return 4*Math.Sin(x) - 0.5*x; }
+        public double Fx(double x)
+        { return -4 * Math.Cos(x) - 0.25 * x * x; }
+        public double h //длинна одного шага
+        {
+            get { return (B - A) / M; }
+        }
+        public double ИнтЛейбниц
+        {
+            get { return Fx(B) - Fx(A); }
+        }
+        public double ИнтТрапеции
+        {
+            get
+            {
+                double sum = (fx(A) + fx(B)) / 2;
+                for (double i = A + h; i < B; i += h) sum += fx(i);
+                sum *= h;
+                return sum;
+            }
+
+        }
+
+        public double ИнтСимпсон
+        {
+            get
+            {
+               
+                double sum = 0.0;
+                double hs = (B - A) / (2*M);
+                double[] xi = new double[2 * M];
+                double[] fxi = new double[2 * M];
+                for (int i = 0; i < 2 * M; i++)
+                {
+                    xi[i] = A + i * hs;
+                    fxi[i] = fx(xi[i]);
+
+                    if ((i > 0) && (i % 2 == 0))
+                        sum += 2*fxi[i];
+                    else
+                        sum += 4 * fxi[i];
+                    
+                }
+                sum += fx(A);
+                sum += fx(B);
+                sum *= hs/3;
+  
+                return sum; 
+            }
+        }
+
+        public double ОстаточныйЧленСимпсона
+        {
+            get
+            {
+                double ostChlen = 0.0;
+                double ksi = 0.0;
+                double hs = (B - A) / (2 * M);
+                double[] xi = new double[2 * M];
+                
+                for (int i = 0; i < 2 * M; i++)
+                {
+                    xi[i] = A + i * hs;
+                    if (Math.Abs(4 * Math.Sin(xi[i])) > ksi) ksi = Math.Abs(4 * Math.Sin(xi[i])); //вычисление кси
+                }
+                ostChlen = (Math.Pow((B - A), 5) / (180 * Math.Pow(2 * M, 4))) * ksi;
+
+                return ostChlen;
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format(" A={0:f2}    B={1:f2}   M={2,4}", A, B, M);
+        }
+    }
+}
